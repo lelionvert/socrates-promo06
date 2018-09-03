040bcffe69901b7229204cf468889aa9f7761ed6
Kevin Kodjo <kevin.kodjo@la-combe-du-lion-vert.fr>
2018-09-03 11:59:59 +0200

    test and implementation when arrival hour is after limit hour there is no cold meal

--- csharp/fr.lacombe.magic6tem/Socrates-Promo6.Test/GetColdMealShould.cs
+++ csharp/fr.lacombe.magic6tem/Socrates-Promo6.Test/GetColdMealShould.cs
@@ -8,13 +8,22 @@ namespace Socrates_Promo6.Test
     public class GetColdMealShould
     {
         [Fact]
-        public void Return_one_cold_meal_when_participant_arrives_before_limit_hour()
+        public void Return_zero_cold_meal_when_arrival_hour_is_before_limit_hour()
         {
             DateTime limitHour = new DateTime(2018, 10, 4, 21, 0, 0);
             DateTime arrivalHour = new DateTime(2018, 10, 4, 20, 0, 0);
 
             int nbColdMeals = GetColdMeal(limitHour, arrivalHour);
             Check.That(nbColdMeals).Equals(0);
+        }
+        [Fact]
+        public void Return_one_cold_meal_when_arrival_hour_is_after_limit_hour()
+        {
+            DateTime limitHour = new DateTime(2018, 10, 4, 21, 0, 0);
+            DateTime arrivalHour = new DateTime(2018, 10, 4, 22, 0, 0);
+
+            int nbColdMeals = GetColdMeal(limitHour, arrivalHour);
+            Check.That(nbColdMeals).Equals(1);
         }
 
 
