0671dd4b55b9ddba0ea0b69a46c94db4765fd492
MaximeRnr <maxime.rnr@gmail.com>
2018-09-03 14:48:48 +0200

    refacto constant to explicit function

--- java/src/main/java/fr/lacombe/magic6tem/Meal.java
+++ java/src/main/java/fr/lacombe/magic6tem/Meal.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class Meal {
-    public static final LocalDateTime THURSDAY_NINE_PM = LocalDateTime.of(2018, 10, 25, 21, 00);
+    public static final LocalDateTime THURSDAY_COLD_DINER_BEGINNING_HOUR = LocalDateTime.of(2018, 10, 25, 21, 00);
     private List<CheckIn> checkIns;
 
     public Meal(List<CheckIn> checkIns) {
@@ -13,7 +13,7 @@ public class Meal {
 
     public int getCountOfColdMeal() {
         return (int) checkIns.stream()
-                .filter(x -> x.arrivalDateTime.isAfter(THURSDAY_NINE_PM))
+                .filter(x -> x.arrivalDateTime.isAfter(THURSDAY_COLD_DINER_BEGINNING_HOUR))
                 .count();
 }
 }
