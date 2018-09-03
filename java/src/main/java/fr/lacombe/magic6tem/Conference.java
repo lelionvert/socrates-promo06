d44fbaed7cb6e973ba1db2df800482274efd19b0
Fabien Labrousse <fabien.labrousse@la-combe-du-lion-vert.fr>
2018-09-03 15:22:01 +0200

    fixed one type issue in Conference

--- java/src/main/java/fr/lacombe/magic6tem/Conference.java
+++ java/src/main/java/fr/lacombe/magic6tem/Conference.java
@@ -2,13 +2,14 @@ package fr.lacombe.magic6tem;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Conference {
 
     private static final LocalTime LIMIT_HOUR = LocalTime.of(21, 0);
-    private ArrayList<Participant> participants;
+    private List<Participant> participants;
 
-    public Conference(ArrayList<Participant> participants) {
+    public Conference(List<Participant> participants) {
         this.participants = participants;
     }
 
