defe4fa50933737a23e07d1f989ad33083a2ef72
Fabien Labrousse <fabien.labrousse@la-combe-du-lion-vert.fr>
2018-09-03 14:12:09 +0200

    returns the number of persons coming after 21h00 by using a list of check in and some Stream::filter

--- java/src/main/java/fr/lacombe/magic6tem/Conference.java
+++ java/src/main/java/fr/lacombe/magic6tem/Conference.java
@@ -1,28 +1,20 @@
 package fr.lacombe.magic6tem;
 
 import java.time.LocalTime;
+import java.util.List;
 
 public class Conference {
 
     private static final LocalTime LIMIT_HOUR = LocalTime.of(21, 0);
-    private LocalTime arrivalTime;
-    private final LocalTime arrivalTime2;
+    private List<LocalTime> arrivalTimes;
 
-    public Conference(LocalTime arrivalTime) {
-        this.arrivalTime = arrivalTime;
-        this.arrivalTime2 = null;
-    }
-
-    public Conference(LocalTime arrivalTime, LocalTime arrivalTime2) {
-        this.arrivalTime = arrivalTime;
-        this.arrivalTime2 = arrivalTime2;
+    public Conference(List<LocalTime> arrivalTimes) {
+        this.arrivalTimes = arrivalTimes;
     }
 
     public int getColdMeals() {
+        return ((int) this.arrivalTimes.stream()
+                .filter(localTime -> localTime.isAfter(LIMIT_HOUR)).count());
 
-        if(arrivalTime.isAfter(LIMIT_HOUR)) {
-            return 1;
-        }
-        return 0;
     }
 }
--- java/src/test/java/fr/lacombe/magic6tem/SocratesTests.java
+++ java/src/test/java/fr/lacombe/magic6tem/SocratesTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,30 +14,42 @@ class GetColdMealShould {
 
     @Test
     public void return_zero_if_one_checked_in_before_21_pm() {
-        LocalTime arrivalDateTime = LocalTime.of(20, 0);
-        Conference conference = new Conference(arrivalDateTime);
+        ArrayList<LocalTime> arrivalTimes = new ArrayList<>();
+        arrivalTimes.add(LocalTime.of(20, 0));
+        Conference conference = new Conference(arrivalTimes);
         int coldMeals = conference.getColdMeals();
         assertThat(coldMeals).isEqualTo(0);
     }
 
     @Test
     public void return_one_if_one_checked_in_after_21_pm() {
-        LocalTime arrivalTime = LocalTime.of(22, 0);
-        Conference conference = new Conference(arrivalTime);
+        ArrayList<LocalTime> arrivalTimes = new ArrayList<>();
+        arrivalTimes.add(LocalTime.of(22, 0));
+        Conference conference = new Conference(arrivalTimes);
         int coldMeals = conference.getColdMeals();
         assertThat(coldMeals).isEqualTo(1);
     }
 
     @Test
     public void return_one_if_two_checked_in_but_only_one_after_21_pm() {
-        LocalTime arrivalTime = LocalTime.of(22, 0);
-        LocalTime arrivalTime2 = LocalTime.of(20, 0);
-        Conference conference = new Conference(arrivalTime,arrivalTime2);
+        ArrayList<LocalTime> arrivalTimes = new ArrayList<>();
+        arrivalTimes.add(LocalTime.of(22, 0));
+        arrivalTimes.add(LocalTime.of(20, 0));
+        Conference conference = new Conference(arrivalTimes);
         int coldMeals = conference.getColdMeals();
         assertThat(coldMeals).isEqualTo(1);
     }
 
-    //TODO : Add test loic!
+    @Test
+    public void return_two_if_two_checked_in_after_21_pm() {
+        ArrayList<LocalTime> arrivalTimes = new ArrayList<>();
+        arrivalTimes.add(LocalTime.of(22, 0));
+        arrivalTimes.add(LocalTime.of(22, 0));
+        Conference conference = new Conference(arrivalTimes);
+        int coldMeals = conference.getColdMeals();
+        assertThat(coldMeals).isEqualTo(2);
+    }
+
 }
 
 
