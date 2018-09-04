9a355cb1cde8da420d4702ddba01a469ab511225
MaximeRnr <maxime.rnr@gmail.com>
2018-09-04 18:12:48 +0200

    added runner to test new feature

--- java/src/main/java/fr/lacombe/magic6tem/Runner.java
+++ java/src/main/java/fr/lacombe/magic6tem/Runner.java
@@ -13,20 +13,71 @@ class Runner {
         String hour;
         String minutes;
         try (Scanner scanner = new Scanner(System.in)) {
-            while (more) {
-                System.out.println("Enter hour : ");
-                hour = scanner.nextLine();
-                System.out.println("Enter minute : ");
-                minutes = scanner.nextLine();
-                checkIns.add(CheckIn.of(LocalDateTime.of(2018, 10, 25, Integer.valueOf(hour), Integer.valueOf(minutes))));
+            System.out.println("What do you want ? ");
+            System.out.println("1 --> Get Cold Meal Given multiple Checkin Hour. ");
+            System.out.println("2 --> Get Covers By Diet Given Participant. ");
+            System.out.println("");
 
-                System.out.println("Do you want some more ? Y / N");
-                more = scanner.nextLine().equals("Y");
+            switch (scanner.nextLine()) {
+                case "1":
+                    while (more) {
+                        System.out.println("Enter hour : ");
+                        hour = scanner.nextLine();
+                        System.out.println("Enter minute : ");
+                        minutes = scanner.nextLine();
+                        checkIns.add(CheckIn.of(LocalDateTime.of(2018, 10, 25, Integer.valueOf(hour), Integer.valueOf(minutes))));
+
+                        System.out.println("Do you want some more ? Y / N");
+                        more = scanner.nextLine().equals("Y");
+
+                    }
+                    Restaurant restaurant = new Restaurant(checkIns);
+                    checkIns.forEach(System.out::println);
+                    System.out.println("Number of Cold Restaurant : " + restaurant.getCountOfColdMeal());
+                    break;
+                case "2":
+                    List<Participant> participants = new ArrayList<>();
+                    while (more) {
+                        System.out.println("Choose Diet  : ");
+                        System.out.println("\t 1 --> VEGETARIAN ");
+                        System.out.println("\t 2 --> VEGAN ");
+                        System.out.println("\t 3 --> PESCARIAN ");
+                        System.out.println("\t 4 --> OMNIVORE ");
+                        System.out.println("");
+                        String dietString = scanner.nextLine();
+                        Diet diet = Diet.OMNIVORE;
+                        Participant participant;
+                        if (dietString.equals("1")) {
+                            diet = Diet.VEGETARIAN;
+                        }
+                        if (dietString.equals("2")) {
+                            diet = Diet.VEGAN;
+                        }
+                        if (dietString.equals("3")) {
+                            diet = Diet.PESCARIAN;
+                        }
+                        if (dietString.equals("4")) {
+                            diet = Diet.OMNIVORE;
+                        }
+                        System.out.println("Participant is Late for Thursday ? Y / N");
+                        if(scanner.nextLine().equals("Y")){
+                            CheckIn checkIn = CheckIn.of(LocalDateTime.of(2018,10,25,22,0));
+                            participant = new Participant(diet,checkIn);
+                            participants.add(participant);
+
+                        } else {
+                            participant = new Participant(diet);
+                            participants.add(participant);
+                        }
+                        System.out.println("Do you want some more ? Y / N");
+                        more = scanner.nextLine().equals("Y");
+                    }
+                    restaurant = new Restaurant();
+                    System.out.println(restaurant.coversFor(participants,6));
+                    break;
             }
         }
-        Restaurant meal = new Restaurant(checkIns);
-        checkIns.forEach(System.out::println);
-        System.out.println("Number of Cold Restaurant : " + meal.getCountOfColdMeal());
+
     }
 
 }
